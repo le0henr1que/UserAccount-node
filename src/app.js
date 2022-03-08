542c4965e99d2fb28b66313b94b7d0a90523424c
le0henr1que <leonardoferreira.henrique1210@gmail.com>
2022-03-08 12:53:30 -0300

    alter now go

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ class App {
 
         this.app.use((req, res, next) => {
             
-            res.header("Access-Controll-Allow-Origin", "*")
+            res.header("Access-Controll-Allow-Origin", "file:///C:/Users/leona/Desktop/Portifolio/Port/index.html")
             res.header("Access-Controll-Allow-Methods", "Get, POST, PUT, DELETE")
             res.header("Access-Controll-Allow-Headers", "Access, Content-type, Authorization, Acept, Origin, X-Requested-width")
             this.app.use(cors())
