8baae59f9cdc06f6e3e95b2b1f53ed6213977f39
le0henr1que <leonardoferreira.henrique1210@gmail.com>
2022-03-08 15:55:03 -0300

    alter now go

--- src/app.js
+++ src/app.js
@@ -18,6 +18,7 @@ class App {
         this.app.use((req, res, next) => {
             
             res.header("Access-Control-Allow-Origin", "*")
+            res.header("Access-Control-Allow-Credentials", true);
             res.header("Access-Control-Allow-Methods", "Get, POST, PUT, DELETE")
             res.header("Access-Control-Allow-Headers", "Access, Content-type, Authorization, Acept, Origin, X-Requested-width")
             this.app.use(cors())
