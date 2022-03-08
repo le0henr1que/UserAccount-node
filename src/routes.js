fff19ce2762f0281fb6cd40d094d3ca4a29504ea
le0henr1que <leonardoferreira.henrique1210@gmail.com>
2022-03-08 11:50:32 -0300

    alter now go

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ const UserController = require("./app/Controllers/UserController")
 const routes = new Router();
 
 routes.post("/store", UserController.store );
-routes.get("/", UserController.show );
+routes.get("/user", UserController.show );
 
 
 module.exports = routes
