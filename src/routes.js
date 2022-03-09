e276446d1f082033227f94a2fcc4156d5416ddbf
le0henr1que <leonardoferreira.henrique1210@gmail.com>
2022-03-09 10:06:15 -0300

    alterando o access control

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,7 @@
 const {Router} = require("express")
 const UserController = require("./app/Controllers/UserController")
-const cors = require('cors')
 
 const routes = new Router();
-
-app.use((req, res, next) => {
-            
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header("Access-Control-Allow-Methods", "Get, POST, PUT, DELETE")
-    res.header("Access-Control-Allow-Headers", "Access, Content-type, Authorization, Acept, Origin, X-Requested-width")
-    app.use(cors())
-    next();
-})
 
 routes.post("/store", UserController.store );
 routes.get("/user", UserController.show );
