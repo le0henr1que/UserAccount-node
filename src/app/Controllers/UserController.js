21a9c0c0f15a8ffff585464d5a6f1a6e8b9bd16a
le0henr1que <leonardoferreira.henrique1210@gmail.com>
2022-03-09 10:00:05 -0300

    alterando o access control

--- src/app/Controllers/UserController.js
+++ src/app/Controllers/UserController.js
@@ -1,17 +1,4 @@
-const app = require('../../app');
 const User = require('../Controllers/Models/User')
-const cors = require('cors')
-
-app.use((re, res, next) =>{
-     
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header("Access-Control-Allow-Methods", "Get, POST, PUT, DELETE")
-    res.header("Access-Control-Allow-Headers", "Access, Content-type, Authorization, Acept, Origin, X-Requested-width")
-    app.use(cors())
-    next();
-})
-
 
 class UserController {
   
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,18 @@
 const {Router} = require("express")
 const UserController = require("./app/Controllers/UserController")
+const cors = require('cors')
 
 const routes = new Router();
+
+routes.use((req, res, next) => {
+            
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Methods", "Get, POST, PUT, DELETE")
+    res.header("Access-Control-Allow-Headers", "Access, Content-type, Authorization, Acept, Origin, X-Requested-width")
+    routes.use(cors())
+    next();
+})
 
 routes.post("/store", UserController.store );
 routes.get("/user", UserController.show );
